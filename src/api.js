41e4c42f42b2de9ada2a924f7e020a6cbe26c814
svadim-s <vadimrazer2001@gmail.com>
2021-03-21 20:43:30 +0300

    adding a websocket

--- src/api.js
+++ src/api.js
@@ -1,40 +1,59 @@
 const API_KEY = "056f0f8287bc4a7b84133d29bf3b9e5a655730191f531ec3abff20f59d0aaefa";
 
 const tickersHandlers = new Map();
+const socket = new WebSocket(
+    `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
+);
 
-export const loadTickers = () => {
-    if (tickersHandlers.size === 0) {
+const AGGREGATE_INDEX = "5";
+
+socket.addEventListener('message', (event) => {
+    const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = JSON.parse(event.data);
+    
+    if (type !== AGGREGATE_INDEX || newPrice === undefined) {
+    return;
+    }
+    
+    const handlers = tickersHandlers.get(currency) ?? [];
+    
+    handlers.forEach(fn => fn(newPrice));
+})
+
+function sendToWebSocket(message) {
+    const stringifiedMessage = JSON.stringify(message);
+
+    if (socket.readyState === WebSocket.OPEN) {
+        socket.send(stringifiedMessage);
         return;
     }
 
-    fetch(
-        `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${[...tickersHandlers.keys()]
-        .join(",")}&tsyms=USD&api_key=${API_KEY}`
-        )
-        .then(r => r.json())
-        .then(rawData => {
-            const updatedPrices = Object.fromEntries(
-                Object.entries(rawData).map(([key, value]) => [key, value.USD])
-            );
+    socket.addEventListener('open', () => {
+        socket.send(stringifiedMessage);
+    }, { once: true});
+}
 
-            Object.entries(updatedPrices).forEach(([currency, newPrice]) => {
-                const handlers = tickersHandlers.get(currency) ?? [];
-                
-                handlers.forEach(fn => fn(newPrice));
-            });
-        });
-};
+function subscribeToTickerOnWs(ticker) {
+    sendToWebSocket({
+      action: "SubAdd",
+      subs: [`5~CCCAGG~${ticker}~USD`]
+    });
+}
 
+function unsubscribeFromTickerOnWs(ticker) {
+    sendToWebSocket({
+      action: "SubRemove",
+      subs: [`5~CCCAGG~${ticker}~USD`]
+    });
+}
+  
 export const subscribeToTicker = (ticker, cb) => {
     const subscribers = tickersHandlers.get(ticker) || [];
 
     tickersHandlers.set(ticker, [...subscribers, cb]);
+    subscribeToTickerOnWs(ticker);
 };
 
 export const unsubscribeFromTicker = ticker => {
     tickersHandlers.delete(ticker);
+    unsubscribeFromTickerOnWs(ticker);
 };
-
-setInterval(loadTickers, 5000);
-
-window.tickers = tickersHandlers;
